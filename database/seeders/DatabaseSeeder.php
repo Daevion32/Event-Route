ee67e4de437cec71cdc048def14ae2bf6c2dc7bc
Inma Mimateach <inmagdiaz92@gmail.com>
2022-08-27 12:35:18 +0200

    user function added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
@@ -24,4 +23,7 @@ class DatabaseSeeder extends Seeder
         Event::factory()->create(['name'=>'Mi evento de prueba','description'=> 'Evento fijado de prueba', 'img' => '', 'spaces'=>'15' , 'adress'=> 'Direccion', 'music gendre'=> 'Tipo musica']);
         Event::factory(50)->create();
     }
+    //User::factory()→create([’name’ ⇒ ‘Admin’, ‘email’ ⇒ ‘admin@admin.com’, ‘password’ ⇒’micontraseña’]);
+
 }
+
