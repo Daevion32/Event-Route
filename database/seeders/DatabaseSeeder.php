32791444ecd1accc72ac155e2e01aa1d0cc0988d
Inma Mimateach <inmagdiaz92@gmail.com>
2022-08-31 13:02:03 +0200

    middleware and conditions for Admin working

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Event;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -75,7 +75,7 @@ class DatabaseSeeder extends Seeder
             'spaces' => '999', 'location' => 'Belgica',
 
         ]);
-        User::factory()->create(['name' => 'Admin', 'email' => 'admin@admin.com', 'isAdmin' => true]);
+        User::factory()->create(['name' => 'admin', 'email' => 'admin@admin.com', 'isAdmin' => true]);
         User::factory()->create(['name' => 'user1', 'email' => 'user1@user1.com', 'isAdmin' => false]);
     }
 }
