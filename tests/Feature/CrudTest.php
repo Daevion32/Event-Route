a1c23b109ef145ee0923d68d6d520de8f2e82999
Francisco <Gimli22@gmail.com>
2022-09-05 14:28:16 +0200

    test inscribe/cancelInscription

--- tests/Feature/CrudTest.php
+++ tests/Feature/CrudTest.php
@@ -93,13 +93,27 @@ class CrudTest extends TestCase
     }
         
 
-     public function test_a_user_can_inscribe(){
+    public function test_a_user_can_inscribe(){
+        $this->withExceptionHandling();
+        $event = Event::factory()->create();
+        $user = User::factory()->create();
+            
+        $response = $this->get(route('inscribe', $user->id ,$event->id));
+        $this->actingAs($user);
+        $response->assertStatus(302);
+                
+        $response->assertSee('');
+    }
+
+    public function test_a_user_can_cancelInscription(){
         $this->withExceptionHandling();
         $user = User::factory()->create();
         $event = Event::factory()->create();
-        $response = $this->get(route('inscribe'), $user->id, $event->id );
-    //     $response->assertStatus(200)
-    //             ->assertViewIs('inscribe');
-    //     $response->assertSee('inscription');
-    // }
+        $response = $this->get(route('cancelInscription', $user->id, $event->id));
+        $this->actingAs($user);
+        $response->assertStatus(302);
+                
+        $response->assertSee('');
+
+    }
 }
